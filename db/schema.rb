fa2350be23eac6da5297cb0f67f6180328d48395
Kaoru Endo <aiandrox7@gmail.com>
2021-05-15 09:55:13 +0900

    rails g scaffold board title:string body:text

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_05_15_005237) do
+
+  create_table "boards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "body", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
 end
---
+++ spec/factories/boards.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :board do
+    title { "MyString" }
+    body { "MyText" }
+  end
+end
